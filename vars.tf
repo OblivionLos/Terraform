772ab076a514bbdf70071e6a269659fe5ad0805f
OblivionLos <45494788+OblivionLos@users.noreply.github.com>
2020-09-07 08:25:02 +0300

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  default = "us-east-2"
+}
+variable "vpc_cidr" {
+  default = "190.160.0.0/16"
+}
+variable "enable_dns_support" {
+  default = "true"
+}
+variable "enable_dns_hostnames" {
+  default = "true"
+}
+variable "aws_id" {
+  default = "AKIARW3JDVDEHDFGDDYH"
+}
+variable "aws_key" {
+  default = "pl6wikndyoRBVtMAnrfYetZlfhjvLL1YdBwGHvak"
+}
+variable "password" {
+  default = "pepega"
+}
